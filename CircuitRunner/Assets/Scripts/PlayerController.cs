9ae5cf3deeb365e562d67c75fe2a38c2cda4a3ea
Juan Ceja <juanceja004@gmail.com>
2019-05-13 13:01:32 -0700

    Create empty project

---
+++ CircuitRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // CONSTANTS
+    public static int kStartNumShields;
+    public static int kStartNumLives;
+
+    public Sprite[] playerSprite;
+
+    public SpriteRenderer playerRenderer;
+
+    public Rigidbody rb;
+
+
+    // Player properties
+    private static bool isDead;
+    private static int numOfLives;
+    private static int numOfShields;
+    public static int NumOfPowerUps { get => numOfLives; set => numOfLives = value; }
+    public static int NumOfShields { get => numOfShields; set => numOfShields = value; }
+    public static bool IsDead { get => isDead; set => isDead = value; }
+
+    void start() 
+    {
+        kStartNumLives = 4;
+        kStartNumShields = 2;
+        isDead = false;
+
+        // Init the num of shields and power ups
+        numOfLives = kStartNumLives;
+        numOfShields = kStartNumShields;  
+
+        rb = GetComponent<Rigidbody> ();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        getKeyBoardInput();
+        
+    }
+
+    void getKeyBoardInput() {
+        // Player controls
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position = new Vector3(this.transform.position.x+5, this.transform.position.y, this.transform.position.z);
+            playerRenderer.sprite = playerSprite[1];
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position = new Vector3(this.transform.position.x-5, this.transform.position.y, this.transform.position.z);
+            playerRenderer.sprite = playerSprite[0];
+        }
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y+5, this.transform.position.z);
+        }
+        
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y-5, this.transform.position.z);
+        }
+    }
+
+    void OnTriggerEnter (Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Shield Pick Up")) 
+        {      
+            if (numOfShields < UIManager.kMaxAmtShields)
+            {
+                numOfShields++;
+            }
+            Destroy(other.gameObject);
+        } 
+        
+        if (other.gameObject.CompareTag ("Battery Pick Up"))
+        {
+            numOfLives++;
+            Destroy(other.gameObject);
+        }  
+
+        if (other.gameObject.CompareTag("Brick Wall"))
+        {
+            isDead = true;
+            playerRenderer.sprite = playerSprite[0];
+        }
+    }
+}
---
+++ CircuitRunner/Assets/Scripts/UIManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // HUD timer, shield, and lightning text reference
+    public Text timerText, shieldUIText, lightningUIText, gameOverText;
+
+    private float startTime, timePassed, timeLeft;
+
+    public Image faderScreen;
+
+    public bool timeFinished;
+
+    private const float kMaxTimeSecsLevel1 = 20; 
+    public const int kMaxAmtShields = 5;
+    public int startAmtShields, startAmtPowerUps;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // get the start time of the level
+       startTime = Time.time; 
+       string minutes = ((int)kMaxTimeSecsLevel1 / 60).ToString();
+       string seconds = (kMaxTimeSecsLevel1 % 60).ToString();
+       string timeLeft = minutes + ": " + seconds;
+       timerText.text = timeLeft;
+       timeFinished = false;
+
+       // set the lightning and shield count 
+       startAmtPowerUps = PlayerController.kStartNumLives;
+       startAmtShields = PlayerController.kStartNumLives;
+       shieldUIText.text = startAmtShields.ToString() + "/" + kMaxAmtShields.ToString();
+       lightningUIText.text = startAmtPowerUps.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+            setShieldAndPowerUpCountText();
+            setTimerText();
+            gameOverUpdate();
+
+        
+    }
+
+    void gameOverUpdate() {
+        if(PlayerController.IsDead) 
+        {
+            Vector4 temp;
+
+            temp = new Vector4(faderScreen.color.r,faderScreen.color.g,faderScreen.color.b,1.0f);
+            faderScreen.color = temp;
+            temp = new Vector4(gameOverText.color.r,gameOverText.color.g,gameOverText.color.b,1.0f);
+            gameOverText.color = temp;
+        }
+    }
+    void setShieldAndPowerUpCountText() {
+        // Get Player Shield amount 
+        shieldUIText.text = PlayerController.NumOfShields.ToString() + "/" + kMaxAmtShields.ToString();
+        lightningUIText.text = PlayerController.NumOfPowerUps.ToString();  
+    }
+    void setTimerText() {
+
+       if (timeFinished) {
+           return;
+       }
+
+       timePassed = Time.time - startTime;
+       timeLeft = kMaxTimeSecsLevel1 - timePassed;
+
+       if (timeLeft <= 0) {
+           timeFinished = true;
+           timerText.color = Color.red;
+           timerText.text = "0: 00";
+
+           // player died
+           PlayerController.IsDead = true;
+           return; 
+       }
+
+       string minutes = ((int)timeLeft / 60).ToString();
+       string seconds = (timeLeft % 60).ToString("f2");
+
+       timerText.text = minutes + ": " + seconds; 
+    }
+}
