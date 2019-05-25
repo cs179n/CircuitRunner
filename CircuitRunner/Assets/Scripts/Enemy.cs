f349fd9ec7fb05bb3a1f0ddc985324fc3a11756e
EdCarrasco <carrasco.edw@gmail.com>
2019-05-25 15:51:55 -0700

    fixed bug with Enemy-RailContainer

--- CircuitRunner/Assets/Scripts/Enemy.cs
+++ CircuitRunner/Assets/Scripts/Enemy.cs
@@ -9,20 +9,22 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (this.target == null) this.target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.isDead = false;
+        this.isDead = false; // TODO: Remove this when Movement script's adjustPosition/Velocity can be disabled
         if (this.isDead) {
+            Debug.Log(this + "DEAD");
             Vector3 currentPos = this.transform.position;
             Vector3 targetPos = this.transform.position + new Vector3(2f, 2f, 0f);
             float stepPos = 30f * Time.deltaTime;
             this.transform.position = Vector3.MoveTowards(currentPos,targetPos,stepPos);
         } else {
             // move enemy
+            
             Vector3 toTarget = (target.position - this.transform.position);
             Vector3 force = toTarget.normalized * 0.001f;
             this.GetComponent<Movement>().addForce(force, true);
--- CircuitRunner/Assets/Scripts/Movement.cs
+++ CircuitRunner/Assets/Scripts/Movement.cs
@@ -14,6 +14,7 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
+        this.railContainer = GameObject.FindGameObjectWithTag("RailContainer");
         Transform rail = this.findClosestRail();
         if (rail) {
             this.currentRail = rail;
