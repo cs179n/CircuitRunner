bb56ec63e60861372d766e7d0deb577a11fa58c8
EdCarrasco <carrasco.edw@gmail.com>
2019-06-06 14:12:42 -0700

    added turret and bullet

---
+++ CircuitRunner/Assets/Turret.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    Transform cannon;
+    public Transform target;
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.cannon = this.transform.GetChild(0).GetChild(0);
+        GameObject bullet = Instantiate(bulletPrefab, this.transform);
+        bullet.transform.position = this.transform.position;
+        bullet.transform.parent = this.transform.parent.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Transform from = this.cannon.transform
+        //this.cannon.rotation = Quaternion.RotateTowards(from, to, step);
+        this.cannon.transform.LookAt(this.target);
+    }
+}
